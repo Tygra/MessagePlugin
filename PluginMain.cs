08dbf87f68d240c1f1ca8155b0b0161e6eb0c704
Kay-P <KayPosmyk@gmx.de>
2015-07-05 01:04:30 +0200

    * Updated to Terraria 1.3. * Added mono debug symbols.

--- PluginMain.cs
+++ PluginMain.cs
@@ -11,7 +11,7 @@ using TShockAPI.DB;
 using System.ComponentModel;
 
 namespace MessagePlugin {
-  [ApiVersion(1, 17)]
+  [ApiVersion(1, 18)]
   public class MessagePlugin : TerrariaPlugin {
     public static List<Message> Messages = new List<Message>();
 
